dbeecf0dcdaeaf78ed7c221dbc49ba18bad2ddea
Mikhail <khromov05@gmail.com>
2020-01-05 22:09:02 +0200

    added class model

--- __init__.py
+++ __init__.py
@@ -1,2 +1,19 @@
+from . import *
 
+
+class TextGenerator:
+	def __init__():
+		pass
+
+	def fit():
+		# process text
+		pass
+
+	def generate():
+		# generate text
+		pass
+
+	def store():
+		# save model to file
+		pass
 
