0979b9c8377110ffb8bf7122c22c1a0a01dac920
khrom <khromov05@gmail.com>
2020-01-18 02:35:32 +0300

    Lots of things. Now using pymorphy2 (only to filter normal_forms, cmon), so no more of this. Added some static parameters to each word, and capability to add more. So now we only need to master the generation of text itself. (lol no, we also need to use sentences)

--- generate.py
+++ generate.py
@@ -37,33 +37,54 @@ def generate(order, length, seed):
     else:
         random.seed(seed)
         l.info('Generating with seed ' + str(seed))
-    sequence = [random.choice(list(order))]
+    sequence = []
+    while True:
+        random_starter_word = random.choice(list(order))
+        if '_' not in random_starter_word:
+            sequence.append(random_starter_word)
+            break
 
-    for counter in range(length):
+    order_static = [order[i]['_static'] for i in list(order) if order[i].get('_static') is not None]
+    tmporder = order.copy()
+    for i in tmporder:
+        try:
+            tmporder[i].pop('_static')
+        except:
+            pass
+    order = tmporder.copy()
+    del tmporder
+
+    for counter in range(length - 1): # cuz we added one already
         try:
             last_used = sequence[-1:][0]
-            l.debug('last_used: ' + last_used)
+            l.debug('last_used: ' + str(order[last_used]))
 
-            tmp_sum = [order[last_used][next_word]['times_repeated'] for next_word in list(order[last_used])]
+            tmp_sum = [order[last_used][next_word]['times_repeated'] for next_word in list(order[last_used])
+                       if type(order[last_used][next_word]) != float]
             tmp_sum = sum(tmp_sum)
             # tmp_sum - how many times word had been used.
 
             item = random.randint(0, tmp_sum)
-            l.debug(order[last_used])
-            l.debug([random.choice(list(order[last_used])) for i in range(len(list(order[last_used])))])
-            for next_word in [random.choice(list(order[last_used])) for i in range(len(list(order[last_used])))]:
+            word_list_randomized = [random.choice(list(order[last_used])) for i in range(len(list(order[last_used])))]
+
+            if len(word_list_randomized) == 0:
+                l.warning('')
+
+            for next_word in word_list_randomized:
                 item -= order[last_used][next_word]['times_repeated']
                 if item <= 0:
                     sequence.append(next_word)
                     l.debug('We added a word ' + next_word + ', because it had chance ' +
-                            str(order[last_used][next_word]['times_repeated'] / tmp_sum))
+                            str(order[last_used][next_word]['times_repeated'] / tmp_sum) +
+                            '. Now breaking the 1-word-finding cycle.')
                     break
                     # TODO: train method adds next_word to next_words, so it is sequence by length of 3. We HAVE to
                     #  use it.
+            l.warning('cycle ended')
         except KeyError as e:
             l.error(str(e) + '; Probably some word had been used in description of other words (like, {"word": {'
-                             '"bad_word"}}), and not mentioned in main order dictionary. Now length is one less.')
-            l.info('Getting another "seed" word.')
+                             '"bad_word"}}), and not mentioned in main order dictionary. Now length is one less. '
+                             'Getting another "seed" word.')
             sequence.append(random.choice(list(order)))
     l.info('generation done, length - ' + str(len(sequence)) + '. Might not be one you typed in.')
 
@@ -80,9 +101,14 @@ def main():
     # parsing arguments from console
     parser = argparse.ArgumentParser(description='Generate text using created model.')
     parser.add_argument('model', help='Path where to get your model.', type=str)
-    parser.add_argument('--length', help='Length of your text in words.', required=False, type=int)
+    parser.add_argument('-l', '--length', help='Length of your text in words.', required=False, type=int)
     parser.add_argument('--seed', help='Seed to generate your model.', required=False, type=int)
+    parser.add_argument('-v', '--verbose', help='Turn on verbose mode', default=False,
+                        action='store_true', required=False)
     args = parser.parse_args()
+
+    if args.verbose == True:
+        l.setLevel(logging.DEBUG)
 
     # generation itself
     order = unpack(args.model)
--- train.py
+++ train.py
@@ -1,8 +1,9 @@
-import random	
+import random
 import argparse
 import pickle
 import logging
 import sys
+import pymorphy2
 
 # TODO: i know it's bad, will have to make this
 # into class and give it self.l variable.
@@ -15,236 +16,244 @@ handler.setFormatter(formatter)
 l.addHandler(handler)
 l.setLevel(logging.DEBUG)
 
+morph = pymorphy2.MorphAnalyzer()
 
 def get_text(path):
-	l.info('getting text from "' + str(path) + '".')
-	try:
-		text = open(path, encoding='utf8').read() # лев толстой, война и мир
-	except FileNotFoundError:
-		l.critical('text file not found')
-		exit()
+    l.info('getting text from "' + str(path) + '".')
+    try:
+        text = open(path, encoding='utf8').read()  # лев толстой, война и мир
+    except FileNotFoundError:
+        l.critical('text file not found')
+        exit()
 
-	return text
+    return text
 
 
 def get_sentense_list(text):
-	# returns something like that: [['word1', 'in', 'sentence', 'one'], ['sentence', 'with', ',', '(comma)'],...] etc
+    # returns something like that: [['word1', 'in', 'sentence', 'one'], ['sentence', 'with', ',', '(comma)'],...] etc
 
-	sentences = []
-	tmp = ''
+    sentences = []
+    tmp = ''
 
-	for i in text:
-		if i == '.' or i == '?' or i == '!' or i == '\n':
-			sentences.append(tmp)
-			tmp = ''
-		else:
-			tmp += i
+    for i in text:
+        if i == '.' or i == '?' or i == '!' or i == '\n':
+            sentences.append(tmp)
+            tmp = ''
+        else:
+            tmp += i
 
-	for i in range(len(sentences)):
-		sentences[i] = get_word_list(sentences[i])
+    for i in range(len(sentences)):
+        sentences[i] = get_word_list(sentences[i])
 
-	return sentences
+    return sentences
 
 
 def get_word_list(text):
+    # TODO: (maybe not in this func) CHECK IF IT IS SOMEONE SPEAKING TO SOMEONE, NOT JUST TEXT. OR IT WILL BE BROKEN.
 
-	# TODO: (maybe not in this func) CHECK IF IT IS SOMEONE SPEAKING TO SOMEONE, NOT JUST TEXT. OR IT WILL BE BROKEN.
+    stop_words = ['нет', 'да', 'не', 'это']
+    alphabet = 'йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЧЯСМИТЬБЮЁ'
+    word_list = [i for i in text.split(' ')]
 
-	alphabet = 'йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЧЯСМИТЬБЮЁ'
-	word_list = [i for i in text.split(' ')]
+    result = []
 
-	result = []
+    index_counter = -1
+    # so it will start from 0. it works, dont touch it.
 
-	index_counter = -1
-	# so it will start from 0. it works, dont touch it.
+    for word in word_list:
+        index_counter += 1
+        tmp = ''
 
-	for word in word_list:
-		index_counter += 1
-		tmp = ''
+        if word in stop_words:
+            continue
 
-		flag = False
-		for letter in word:
-			if letter in alphabet or letter == ',':
-				tmp += letter
-				if letter == ',':
-					tmp = tmp[:-1]
-					flag = True
+        flag = False
+        for letter in word:
+            if letter in alphabet or letter == ',':
+                tmp += letter
+                if letter == ',':
+                    tmp = tmp[:-1]
+                    flag = True
 
-		# counter variable is replacement for word_list.index(i)
-		result.append(tmp)
-		if flag:
-			result.append(',')
-		
-		# now it adds not only words, but also commas as separate words.
+        # counter variable is replacement for word_list.index(i)
+        result.append(tmp)
+        if flag:
+            result.append(',')
 
-	result = [i for i in result if i != '']
-	return result
+    # now it adds not only words, but also commas as separate words.
+
+    result = [i for i in result if i != '']
+    return result
 
 
 def dump(model_path, order):
-	with open(model_path, 'wb') as f:
-		pickle.dump(order, f)
-	l.info('writing down to "' + str(model_path) + '" done.')
+    with open(model_path, 'wb') as f:
+        pickle.dump(order, f)
+    l.info('writing down to "' + str(model_path) + '" done.')
 
 
 def get_regular_case(word):
-
-	# TODO: make it work, otherwise it is kinda useless.
-
-	return word
+    # returns normal form of any word.
+    return morph.parse(word)[0].normal_form
 
 
 def get_word_order(sentence_list):
-	# returns:
-	#
-	# {'word': {'second_word1': {'times_repeated': 100, 'next_words': {'third_word': 2, 'third_word2': 1}},
-	# 'second_word2': {'times_repeated': 99, 'next_words': {'third_word': 12, 'third_word2': 11}}, ...}
+    l.info('getting word order')
+    # returns:
+    #
+    # {'word': {'second_word1': {'times_repeated': 100, 'next_words': {'third_word': 2, 'third_word2': 1}},
+    # 'second_word2': {'times_repeated': 99, 'next_words': {'third_word': 12, 'third_word2': 11}}, ...}
 
-	# TODO: make it use sentense_list, not word_list, so it
-	# will be able to cut proper nouns. And use commas?
+    # TODO: make it use sentense_list, not word_list, so it
+    # will be able to cut proper nouns. And use commas?
 
-	order = {}
-	counter = 0
-	some_constant_which_we_need_to_have = 3
-	# todo: add that constant properly and etc
+    order = {}
+    counter = 0
+    some_constant_which_we_need_to_have = 3
+    # todo: add that constant properly and etc
 
-	for word_list in sentence_list:
-		for word_id in range(len(word_list)-1):
-			word = word_list[word_id]
-			word = get_regular_case(word)
-			word_pos = (word_id + 1) / len(word_list) # normalized position in sentence
+    for word_list in sentence_list:
 
+        # seeding
 
-			if word == word.capitalize() and word_id != 0 and word != ',':
-				# if this is capitalised and not first word in sentense, then ignore it,
-				# because we don't need proper nouns.
-				continue
-			word = word.lower()
-			counter += 1
+        for word_id in range(len(word_list) - 1):
+            word = word_list[word_id]
+            word = get_regular_case(word)
+            word_pos = (word_id + 1) / len(word_list)  # normalized position in sentence
 
-			try:
-				next_word = word_list[word_id + 1]
-				next_next_word = word_list[word_id + 2]
-				if word_list[word_id + 2] == word_list[word_id + 2].capitalize():
-					next_next_word = ''
-				if word_list[word_id + 1] == word_list[word_id + 1].capitalize():
-					next_word = ''
+            if word == word.capitalize() and word_id != 0 and word != ',':
+                # if this is capitalised and not first word in sentense, then ignore it,
+                # because we don't need proper nouns.
+                continue
+            word = word.lower()
+            counter += 1
 
-			except IndexError:
+            try:
+                next_word = word_list[word_id + 1]
+                next_next_word = word_list[word_id + 2]
+                if word_list[word_id + 2] == word_list[word_id + 2].capitalize():
+                    next_next_word = ''
+                if word_list[word_id + 1] == word_list[word_id + 1].capitalize():
+                    next_word = ''
 
-				# TODO: remove continue and replace it with something
-				# else, because this cuts some words.
-				continue
-			try:
-				order[word]['_position'] = (order[word]['position'] + word_pos) / 2
-				order[word][next_word]['times_repeated'] += 1
-				order[word][next_word]['next_words'][next_next_word] += 1
-			except KeyError as e3:
-				# l.debug(str(e3) + ' e3')
-				try:
-					order[word]['_position'] = (order[word]['position'] + word_pos) / 2
-					order[word][next_word]['times_repeated'] += 1
-					order[word][next_word]['next_words'].update({next_next_word: 1})
-				except KeyError as e2:
-					# l.debug(str(e2) + ' e2')
-					try:
-						# here and in next "try:" var.update({smthing: {}}) is equal to var[smthing] = {...}
-						order[word]['_position'] = (order[word]['position'] + word_pos) / 2
-						order[word].update({next_word: {
-							'times_repeated': 1, 'next_words': {next_next_word: 1}}
-						})
-					except KeyError as e:
-						# l.debug(str(e) + ' e')
-						order.update({word: {next_word: {
-							'times_repeated': 1, 'next_words': {next_next_word: 1}}
-						}})
-						order[word]['_position'] = word_pos
+            except IndexError:
 
-	# if word has been mentioned very not often after
-	# some word, we delete it, because maybe it was 1 situation,
-	# not an order we can follow.
-	tmporder = order.copy()
+                # TODO: remove continue and replace it with something
+                # else, because this cuts some words.
+                continue
+            try:
+                order[word]['_static']['pos'] = (order[word]['position'] + word_pos) / 2
+                order[word][next_word]['times_repeated'] += 1
+                order[word][next_word]['next_words'][next_next_word] += 1
+            except KeyError as e3:
+                # l.debug(str(e3) + ' e3')
+                try:
+                    order[word]['_static']['pos'] = (order[word]['position'] + word_pos) / 2
+                    order[word][next_word]['times_repeated'] += 1
+                    order[word][next_word]['next_words'].update({next_next_word: 1})
+                except KeyError as e2:
+                    # l.debug(str(e2) + ' e2')
+                    try:
+                        # here and in next "try:" var.update({smthing: {}}) is equal to var[smthing] = {...}
+                        order[word]['_static']['pos'] = (order[word]['position'] + word_pos) / 2
+                        order[word].update({next_word: {
+                            'times_repeated': 1, 'next_words': {next_next_word: 1}}
+                        })
+                    except KeyError as e:
+                        # l.debug(str(e) + ' e')
+                        order.update({word: {next_word: {
+                            'times_repeated': 1, 'next_words': {next_next_word: 1}}
+                        }})
+                        order[word]['_static'] = {'pos': word_pos}
 
-	# {'word': {'second_word1': {'times_repeated': 100, 'next_words': {'third_word': 2, 'third_word2': 1}},
-	# 'second_word2': {'times_repeated': 99, 'next_words': {'third_word': 12, 'third_word2': 11}}, ...}
-	for word, word_val in order.items():
-		if word_val == {}:
-			tmporder.pop(word)
-			continue
-		if word == '' or word == ',':
-			tmporder.pop(word)
-			continue
-		for nxt_word in list(word_val):
-			nxt_word_val = word_val[nxt_word]
-			if type(nxt_word_val) == float:
-				# reserved thing, for key '_position'
-				continue
-			if nxt_word_val == {}:
-				tmporder[word].pop(nxt_word)
-				continue
-			if nxt_word == '' or nxt_word == ',':
-				tmporder[word].pop(nxt_word)
-				continue
-			for nxt2_word in list(nxt_word_val['next_words']):
-				nxt2_word_val = nxt_word_val['next_words'][nxt2_word]
-				if nxt2_word_val == {}:
-					tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
-				if nxt2_word == '' or nxt2_word == ',':
-					tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
-				elif nxt2_word_val < some_constant_which_we_need_to_have:
-					tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
+    # if word has been mentioned very not often after
+    # some word, we delete it, because maybe it was 1 situation,
+    # not an order we can follow.
 
-	order = tmporder.copy()
-	for word, word_val in order.items():
-		if word_val == {}:
-			tmporder.pop(word)
-			continue
-		if word == '' or word == ',':
-			tmporder.pop(word)
-			continue
-		for nxt_word in list(word_val):
-			nxt_word_val = word_val[nxt_word]
-			if type(nxt_word_val) == float:
-				# reserved thing, for key '_position'
-				continue
-			if nxt_word_val == {}:
-				tmporder[word].pop(nxt_word)
-				continue
-			if nxt_word == '' or nxt_word == ',':
-				tmporder[word].pop(nxt_word)
-				continue
-			for nxt2_word in list(nxt_word_val['next_words']):
-				nxt2_word_val = nxt_word_val['next_words'][nxt2_word]
-				if nxt2_word_val == {}:
-					tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
-				if nxt2_word == '' or nxt2_word == ',':
-					tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
-				elif nxt2_word_val < some_constant_which_we_need_to_have:
-					tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
-	order = tmporder.copy()
+    order_static = [[i, order[i]['_static']] for i in list(order) if order[i].get('_static') is not None]
+    tmporder = order.copy()
+    for i in tmporder:
+        try:
+            tmporder[i].pop('_static')
+        except:
+            pass
+    order = tmporder.copy()
+    for word, word_val in order.items():
+        if word_val == {}:
+            tmporder.pop(word)
+            continue
+        if word == '' or word == ',':
+            tmporder.pop(word)
+            continue
+        for nxt_word in list(word_val):
+            nxt_word_val = word_val[nxt_word]
+            if nxt_word_val == {}:
+                tmporder[word].pop(nxt_word)
+                continue
+            if nxt_word == '' or nxt_word == ',':
+                tmporder[word].pop(nxt_word)
+                continue
+            for nxt2_word in list(nxt_word_val['next_words']):
+                nxt2_word_val = nxt_word_val['next_words'][nxt2_word]
+                if nxt2_word_val == {}:
+                    tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
+                if nxt2_word == '' or nxt2_word == ',':
+                    tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
+                elif nxt2_word_val < some_constant_which_we_need_to_have:
+                    tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
 
-	return order
+    order = tmporder.copy()
+    for word, word_val in order.items():
+        if word_val == {}:
+            tmporder.pop(word)
+            continue
+        if word == '' or word == ',':
+            tmporder.pop(word)
+            continue
+        for nxt_word in list(word_val):
+            nxt_word_val = word_val[nxt_word]
+            if nxt_word_val == {}:
+                tmporder[word].pop(nxt_word)
+                continue
+            if nxt_word == '' or nxt_word == ',':
+                tmporder[word].pop(nxt_word)
+                continue
+            for nxt2_word in list(nxt_word_val['next_words']):
+                nxt2_word_val = nxt_word_val['next_words'][nxt2_word]
+                if nxt2_word_val == {}:
+                    tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
+                if nxt2_word == '' or nxt2_word == ',':
+                    tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
+                elif nxt2_word_val < some_constant_which_we_need_to_have:
+                    tmporder[word][nxt_word]['next_words'].pop(nxt2_word)
+
+    order = tmporder.copy()
+
+    for i in order_static:
+        if i[0] in list(order):
+            order[i[0]].update({'_static': i[1]})
+
+    return order
 
 
 def main():
-	# parsing arguments
-	parser = argparse.ArgumentParser(description='Teach your model.', prog='python train.py')
-	parser.add_argument('input_dir',  help='Path to text file.', type=str)
-	parser.add_argument('model', help='Path to where to save your model.', type=str)
-	args = parser.parse_args()
+    # parsing arguments
+    parser = argparse.ArgumentParser(description='Teach your model.', prog='python train.py')
+    parser.add_argument('input_dir', help='Path to text file.', type=str)
+    parser.add_argument('model', help='Path to where to save your model.', type=str)
+    args = parser.parse_args()
 
-	text = get_text(args.input_dir)
-	sentense_list = get_sentense_list(text)
-	order = get_word_order(sentense_list)
-	l.debug('order len - ' + str(len(list(order))))
+    text = get_text(args.input_dir)
+    sentense_list = get_sentense_list(text)
+    order = get_word_order(sentense_list)
+    l.debug('order len - ' + str(len(list(order))))
 
-	l.info('training done')
-	dump(args.model, order)
+    l.info('training done')
+    dump(args.model, order)
 
 
 if sys.argv[0] == 'train.py' or sys.argv[0] == 'D:/Programming/textgen/train.py':
-	try:
-		main()
-	except KeyboardInterrupt:
-		exit('\n\nKeyboard Interrupt')
-
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit('\n\nKeyboard Interrupt')
